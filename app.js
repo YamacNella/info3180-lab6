d842c211c33dd4e9e133c232de24ec3b2706b786
Camay <camayall_95@yahoo.com>
2017-03-29 15:11:29 +0000

    step 3

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ app.controller("myCtrl", function($scope) {
     $scope.products = ["Milk", "Bread", "Cheese", "Soap", "Cereal", "Baby Oil"];
     $scope.addItem = function () {
         $scope.products.push($scope.addMe);
-    } 
+    }; 
+    $scope.removeItem = function (x) {
+        $scope.products.splice(x, 1);
+    }; 
 });
