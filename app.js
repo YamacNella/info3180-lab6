8f4f4be8aceae2c13e27a6e003ac6a1dfb54bd4d
Camay <camayall_95@yahoo.com>
2017-03-29 15:13:15 +0000

    step 4

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var app = angular.module("myShoppingList", []); 
+
 app.controller("myCtrl", function($scope) {
     $scope.products = ["Milk", "Bread", "Cheese", "Soap", "Cereal", "Baby Oil"];
+    
     $scope.addItem = function () {
-        $scope.products.push($scope.addMe);
-    }; 
+        $scope.errortext = "";
+        if (!$scope.addMe) {return;}
+        if ($scope.products.indexOf($scope.addMe) == -1) {
+            $scope.products.push($scope.addMe);
+        } else {
+            $scope.errortext = "The item is already in your shopping list.";
+        }
+    };
+    
     $scope.removeItem = function (x) {
         $scope.products.splice(x, 1);
     }; 
